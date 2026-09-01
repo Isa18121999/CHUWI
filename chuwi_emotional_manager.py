2641e1be5838fe3c7fb565439112d05ccb973e65
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-09-01 12:50:15 -0500

    Integrate emotional database with manager

--- chuwi_emotional_manager.py
+++ chuwi_emotional_manager.py
@@ -1,15 +1,17 @@
 from chuwi_emotional_adaptation import ChuwiEmotionalAdaptation
+from emotion_database import EmotionDatabase
 
 
 class ChuwiEmotionalManager:
-    """Coordina detección emocional y estrategia de interacción de Chuwi."""
+    """Coordina deteccion emocional, base de conocimiento y estrategia de interaccion."""
 
     def __init__(self):
         self.adapter = ChuwiEmotionalAdaptation()
+        self.database = EmotionDatabase()
 
     def analyze_interaction(self, message):
         emotion = self.adapter.detect_emotion(message)
-        strategy = self.adapter.select_strategy(emotion)
+        strategy = self.database.get_strategy(emotion)
 
         return {
             "emotion": emotion,
