1b606e74d09bf3024973acc9c8bcb32bb008f29f
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-09-01 20:26:45 -0500

    refactor: move chuwi controller into src robot

---
+++ src/robot/chuwi_controller.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class ChuwiState(Enum):
+    IDLE = "Esperando"
+    DETECTED = "Persona detectada"
+    WELCOME = "Bienvenida"
+    LISTENING = "Escuchando"
+    THINKING = "Procesando"
+    SPEAKING = "Respondiendo"
+
+
+class ChuwiController:
+
+    def __init__(self):
+        self.state = ChuwiState.IDLE
+        self.distance = None
+
+    def update_distance(self, distance):
+        self.distance = distance
+
+        if distance <= 80:
+            if self.state == ChuwiState.IDLE:
+                self.change_state(ChuwiState.DETECTED)
+        else:
+            self.change_state(ChuwiState.IDLE)
+
+    def start_welcome(self):
+        self.change_state(ChuwiState.WELCOME)
+
+    def start_listening(self):
+        self.change_state(ChuwiState.LISTENING)
+
+    def start_thinking(self):
+        self.change_state(ChuwiState.THINKING)
+
+    def start_speaking(self):
+        self.change_state(ChuwiState.SPEAKING)
+
+    def change_state(self, new_state):
+        self.state = new_state
+
+    def get_state(self):
+        return self.state.value
