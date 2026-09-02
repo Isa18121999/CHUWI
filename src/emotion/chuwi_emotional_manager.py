5976a33a34dd8b1784a4dff2e88a18a947a2f071
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-09-02 08:43:10 -0500

    fix: map anxious emotion to database

--- src/emotion/chuwi_emotional_manager.py
+++ src/emotion/chuwi_emotional_manager.py
@@ -5,11 +5,16 @@ from src.emotion.emotion_database import EmotionDatabase
 class ChuwiEmotionalManager:
     """Coordina deteccion emocional, base de conocimiento y estrategia de interaccion."""
 
+    EMOTION_DB_MAP = {
+        "ANXIOUS": "anxiety",
+    }
+
     def __init__(self):
         self.adapter = ChuwiEmotionalAdaptation()
         self.database = EmotionDatabase()
 
     def analyze_interaction(self, message):
         emotion = self.adapter.detect_emotion(message)
-        strategy = self.database.get_strategy(emotion.lower())
+        database_emotion = self.EMOTION_DB_MAP.get(emotion, emotion.lower())
+        strategy = self.database.get_strategy(database_emotion)
         return {"emotion": emotion, "strategy": strategy}
